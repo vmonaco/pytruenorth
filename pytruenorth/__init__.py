26330949064a1bde8aff31c12740511281028274
Vinnie Monaco <vinmonaco@gmail.com>
2016-07-14 21:09:58 -0600

    Fixed typos in __init__.py

--- pytruenorth/__init__.py
+++ pytruenorth/__init__.py
@@ -1,8 +1,3 @@
+from .truenorth import TrueNorthChip, NEURON_DEST_OUT
 
-import os
-ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-CACHE_DIR = os.path.join(ROOT_DIR, 'cache')
-
-from .mnist import load as load_mnist
-from .cmu import load as load_cmu
-from .biosig import load as load_biosig
+__all__ = ['TrueNorthChip', 'NEURON_DEST_OUT']
